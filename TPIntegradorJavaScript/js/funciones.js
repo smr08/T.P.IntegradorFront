6812f3f9d4843e71167e52834ad97b9ac96e9a24
Sofia <sofiamariarossi@gmail.com>
2023-06-10 13:18:37 -0300

    Agregado de javaScript a la pagina de tickets

---
+++ TPIntegradorJavaScript/js/funciones.js
@@ -0,0 +1,32 @@
+
+
+let cantidadEntradas = document.getElementById('cantidadEntrada');
+ 
+let categoria = document.getElementById('opcionCategoria');
+
+let botonResumen = document.getElementById('botonResumen');
+
+let totalCompra = document.getElementById('total');
+
+function resumen() {
+
+    if (categoria.value == "Estudiante") {
+        totalCompra =  200 * cantidadEntradas.value * 0.2;
+    } else if (categoria.value == "Trainee") {
+        totalCompra =  200 * cantidadEntradas.value * 0.5;
+    } else if (categoria.value == "Junior") {
+        totalCompra =  200 * cantidadEntradas.value * 0.85;
+    } else {
+        console.log("usted no seleccionó nada")
+       
+    }
+    
+    console.log(totalCompra);
+
+
+
+    document.getElementById("total").innerHTML = `Total a pagar: $ ${totalCompra}`; 
+    //   document.querySelector("#total").innerHTML = `Total a pagar: $ ${totalCompra}`; 
+
+}
+
